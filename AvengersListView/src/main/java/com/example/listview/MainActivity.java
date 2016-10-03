8f8fbfeb84b4c81bf797eab3e4a010f8f2b765ed
vivek50 <vivekmodi50>
2016-10-03 14:57:34 +0530

    Merge branch 'master' of /home/vivek50/AndroidStudioProjects/MyApplication with conflicts.

--- AvengersListView/src/main/java/com/example/listview/MainActivity.java
+++ AvengersListView/src/main/java/com/example/listview/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Collections;
 
@@ -51,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
 
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 
@@ -61,6 +62,22 @@ public class MainActivity extends AppCompatActivity {
             imageView.setImageResource(images[position]);
             TextView textView = (TextView) view.findViewById(R.id.textview);
             textView.setText(titles[position]);
+
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(position==0)
+                    {
+                        Toast.makeText(getContext(), "Postion" + position, Toast.LENGTH_SHORT).show();
+                    }
+                    else if (position==1)
+                    {
+                        Toast.makeText(getContext(), "Postion" + position, Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
 
             return view;
         }
